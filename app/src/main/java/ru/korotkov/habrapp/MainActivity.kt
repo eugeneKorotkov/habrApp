70e0e47473725696a028af0542b932f1e8df8bfc
Eugene <work.eugene.korotkov@gmail.com>
2020-03-02 16:23:06 +0300

    first commit

---
+++ app/src/main/java/ru/korotkov/habrapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package ru.korotkov.habrapp
+
+import android.annotation.TargetApi
+import android.os.Build
+import android.os.Bundle
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                view?.loadUrl(url)
+                return true
+            }
+        }
+        webView.loadUrl("https://m.habr.com/ru/all/")
+    }
+}
